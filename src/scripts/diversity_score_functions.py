a4396ebf54bf25d2207c097dce7b9e514849f84d
yoyo1401 <151439439+yoyo1401@users.noreply.github.com>
2024-11-15 19:15:47 +0100

    added scripts for diversity score

--- src/scripts/diversity_score_functions.py
+++ src/scripts/diversity_score_functions.py
@@ -1,7 +1,7 @@
-import pandas as pd
-import numpy as np
-
 def calculate_gender_diversity(df):
+    #This function computes gender diversity in a list of aactors
+    #inputs: df with column actor genders as 'M' or 'F'
+    #outputs: scalar value  gender diversity
     count_male = (df['Actor Gender'] == 'M').sum()
     count_female = (df['Actor Gender'] == 'F').sum()
     total_count = count_male+count_female
@@ -12,10 +12,12 @@ def calculate_gender_diversity(df):
 
 
 def calculate_ethnicity_diversity(df):
-    '''
-    Function to calculate ethnicity diversity using Simpson's diversity index, which is a common measure for quantifying diversity.
+   
+    #This Function to calculate ethnicity diversity using Simpson's diversity index,
+    #  which is a common measure for quantifying diversity.
+    #inputs: df with column Actor Ethnicity as 'string'
+    #outputs: scalar value  gender diversity
 
-    '''
     ethnicity_counts = df['Actor Ethnicity'].value_counts()
     total_count = len(df)
     proportions = ethnicity_counts / total_count
@@ -24,6 +26,10 @@ def calculate_ethnicity_diversity(df):
 
 
 def calculate_age_diversity(df):
+    #This Function to calculate age  diversity using Simpson's diversity index,
+    #inputs: df with column Actor Age as int
+    #outputs: scalar value age diversity
+
     age_bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     age_labels = ['0-10', '10-20', '20-30', '30-40', '40-50', '50-60', '60-70', '70-80', '80-90', '90-100']
     df['Age Range'] = pd.cut(df['Actor Age'], bins=age_bins, labels=age_labels, right=False)
@@ -41,10 +47,9 @@ def calculate_age_diversity(df):
     return age_diversity
 
 def calculate_height_diversity(df):
-    '''
-    Function to calculate height diversity using Simpson's diversity index, which is a common measure for quantifying diversity.
-
-    '''
+    #This Function to calculate height diversity using Simpson's diversity index,
+    #inputs: df with column Actor Height as double
+    #outputs: scalar value height diversity
     height_counts = df['Actor Height'].value_counts()
     total_count = len(df)
 
@@ -63,10 +68,9 @@ def calculate_height_diversity(df):
     return height_diversity
 
 def calculate_foreign_actor_proportion(df):
-    '''
-    Function to calculate foreign action proportion of actors whose country of origin is differnet from the movie country.
-
-    '''
+    #This Function to calculate foreign actor proportion
+    #inputs: df with column Movie Country as List, 'Actor Country of Origin' as string, Freebase Movie Id as string
+    #outputs: scalar value foreign actor proportion
     proportions = []
     for movie_id, group in df.groupby('Freebase Movie ID'):
         movie_countries = set(group['Movie Country'].iloc[0])  # Get unique movie country list
@@ -80,3 +84,31 @@ def calculate_foreign_actor_proportion(df):
         })
     result_df = pd.DataFrame(proportions)
     return result_df
+
+
+movie_info = df_metadata_OI_exploded.groupby('Freebase Movie ID').agg({
+    'Movie Country': lambda x: list(x.unique()),  
+    'Movie Language': lambda x: list(x.unique()), 
+    'Movie Release Date': 'first',                
+    'Movie Box Office Revenue': 'first'           
+}).reset_index()
+
+
+df_merged_1 = df_metadata_OI_exploded.drop(columns=['Movie Country', 'Movie Language', 'Movie Release Date', 'Movie Box Office Revenue']) \
+                          .merge(movie_info, on='Freebase Movie ID', how='left')
+
+
+df_merged_1 = df_merged_1[[
+    'Freebase Movie ID', 'Movie Country', 'Movie Language', 'Movie Release Date', 'Movie Box Office Revenue',
+    'Actor Age', 'Actor Gender', 'Actor Ethnicity', 'Actor Height', 'Actor Country of Origin'
+]]
+
+
+df_merged_unique = df_merged_1.drop_duplicates(subset=[
+    'Freebase Movie ID', 'Actor Height', 'Actor Ethnicity', 'Actor Age', 'Actor Gender', 'Actor Country of Origin'
+], keep='first')
+
+count = df_merged_unique['Freebase Movie ID'].value_counts().get('/m/011yfd', 0)
+
+df_metadata_OI = df_merged_unique[df_merged_unique['Freebase Movie ID'].map(df_merged_unique['Freebase Movie ID'].value_counts()) > 1]
+df_metadata_OI.head(100)
